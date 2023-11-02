79edfe2c19364e6d99ce2ce5ce12ba5c0fb84b8a
Victor-Pillon <victor.pll2001@gmail.com>
2023-11-01 21:50:08 -0300

    Created define file to specify structures

--- inc/Defines.h
+++ inc/Defines.h
@@ -1,3 +1,27 @@
+#include <vector>
+#include <memory>
+#include <random>
+#include <stdlib.h>
+#include <iostream>
+
+#include "ContextListSearch.h"
+#include "ContextListSort.h"
+
+#include "StrategyBubbleSort.h"
+#include "StrategyImprovedBubbleSort.h"
+#include "StrategyInsertionSort.h"
+#include "StrategySelectionSort.h"
+#include "StrategyMergeSort.h"
+#include "StrategyQuickSort.h"
+#include "StrategyHeapSort.h"
+
+#include "StrategyBinarySearch.h"
+#include "StrategyLinearSearch.h"
+
+#include "ProxyListSearchTimer.h"
+#include "ProxyListSortTimer.h"
+
+
 enum Menu {
 	QUANTITY = 0,
 	ORDER,
@@ -30,3 +54,18 @@ enum AlgorithmOptions {
 	ALL_ALGORITHMS,
 	BACK_ALGORITHMS
 };
+
+ContextListSort sorterContext;
+ContextListSearch searchContext;
+ProxyListSearchTimer* searchProxy;
+ProxyListSortTimer* sortProxy; 
+
+std::vector<int> randomVector;
+std::vector<int> orderedVector;
+std::vector<int> disorderedVector;
+
+Menu menu = Menu::QUANTITY;
+int tempOption;
+QuantityOptions selectedQuantity = QuantityOptions::ALL_QUANTITIES;
+OrderOptions selectedOrder = OrderOptions::ALL_ORDERS;
+AlgorithmOptions selectedAlgorithm = AlgorithmOptions::ALL_ALGORITHMS;
